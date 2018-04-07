ce60ef5e33f3d413e4d577921d368193da972204
Della Apella <dellapella13@gmail.com>
2018-04-07 18:18:14 +0700

    yey

--- TiketPesawat.java
+++ TiketPesawat.java
@@ -1,20 +1,33 @@
-//KKKKKK
-//SUs mau gak
-<<<<<<< HEAD
-//Coba punya della yaaaaa
 
-=======
-//nyambek 17
->>>>>>> 751b960ab3d381534a89b0d2a8f32369adad7b06
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class TiketPesawat {
+
+	static int datarute[][] = new int[1][4]; //asal, tujuan, jml penumpang, kelas
+	static int temp_rute[][] = new int [1][4];
+        
+        static int datapbl[][] = new int[1][5]; //nama, tgl_lahir, jenkel, no_telp, email 
+        static int temp_pbl[][] = new int [1][5];
+        
+	static int dataplg[][] = new int[1][3]; //nama, tgl_lahir, jenkel
+	static int temp_plg[][] = new int [1][3];
+
+	static int datatrk[][] = new int [1][];
+	static int temp_trk[][] = new int [1][];
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 	public static void main(String[] args) {
-		// do{
+
+
+		do{
 			System.out.println("Menu Utama");
-			System.out.println("1. Input Rute");
-			System.out.println("2. Transaksi");
-			System.out.println("3. Tampil History Pembelian");
-			System.out.println("4. Exit");
-			System.out.println("4. Exisaaasfsa");
-		// }while(true);
+			System.out.println("1. Penerbangan");
+			System.out.println("2. Tampil History Pembelian");
+			System.out.println("3. Exit");
+
+
+		}while(true);
 	}
 }
