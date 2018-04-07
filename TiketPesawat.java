da3a3c2c5d2bcea62302e0b5cdcf4274f814d67f
Denandra Prasetya Laksma Putra <denandra1628@gmail.com>
2018-04-07 17:32:08 +0700

    TiketPesawat.java

--- TiketPesawat.java
+++ TiketPesawat.java
@@ -1,5 +1,11 @@
 public class TiketPesawat {
 	public static void main(String[] args) {
-		
+		// do{
+			System.out.println("Menu Utama");
+			System.out.println("1. Input Rute");
+			System.out.println("2. Transaksi");
+			System.out.println("3. Tampil History Pembelian");
+			System.out.println("4. Exit");
+		// }while(true);
 	}
 }
