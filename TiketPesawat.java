39874c8a622bacef255583dac2965448ceaefa68
Denandra Prasetya Laksma Putra <denandra1628@gmail.com>
2018-04-07 17:53:57 +0700

    mau gak?

--- TiketPesawat.java
+++ TiketPesawat.java
@@ -1,20 +1,28 @@
-//KKKKKK
-//SUs mau gak
-<<<<<<< HEAD
-//Coba punya della yaaaaa
-
-=======
-//nyambek 17
->>>>>>> 751b960ab3d381534a89b0d2a8f32369adad7b06
 public class TiketPesawat {
 	public static void main(String[] args) {
-		// do{
+		do{
 			System.out.println("Menu Utama");
 			System.out.println("1. Input Rute");
 			System.out.println("2. Transaksi");
 			System.out.println("3. Tampil History Pembelian");
 			System.out.println("4. Exit");
 			System.out.println("4. Exisaaasfsa");
-		// }while(true);
+		}while(true);
+	}
+
+	static void input(){
+
+	}
+
+	static void transaksi(){
+
+	}
+
+	static void history(){
+
+	}
+
+	static void exit(){
+		
 	}
 }
